f4111a9eab152580b0893ec2fab483d154a32eb5
Khalishah Nadhirah <khalishahnadhirah@gmail.com>
2021-08-24 18:07:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,24 @@ def read(file_name):
     with io.open(os.path.join(os.path.dirname(__file__), file_name),
                  encoding='utf-8') as f:
         return f.read()
+
+setup(
+    name='dynamic_deephit',
+    url='https://github.com/khalishah/Dynamic-DeepHit',
+    author='Khalishah',
+    author_email='khalishah@benshi.ai',
+    packages=['dynamic_deephit'],
+    install_requires=['funcsigs',
+        'numpy',
+        'pandas',
+        'tensorflow',
+        'scikit-learn',
+        'lifelines',
+        'termcolor'
+        'scikit-survival']
+    ,
+    description='Dynamic-DeepHit',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+)
     
